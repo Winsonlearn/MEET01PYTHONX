3d31fbfad4ef4b06793099e7e80c6a749cb80dce
winson <winsonlearn@gmail.com>
2025-01-17 10:13:01 +0700

    update

--- main.py
+++ main.py
@@ -4,16 +4,31 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/home")
 def home():
-    name = request.args.get('name', '?')
-    age = int(request.args.get('age', '?'))
-    return render_template('home.html', name = name, age = age)
-     
+    if 'name' in request.args.keys() and 'umur' in request.args.keys():
+        name = request.args.get('name', '?')
+        age = int(request.args.get('age', '?'))
+        return render_template('home.html', name = name, age = age)
+    elif request.args["name"] == "budi":
+        return redirect("/contact1")
+    elif request.args["name"] == "santoso":
+        return redirect(url_for("contact1"))
+    else:
+        return f"404 Not Found"
+
+@app.route("/contact1")
+def contact1():
+    return "<h1>Contact Page</h1>"
+    
+@app.route("/tester/<string:kata>/<int:angka>/<terserah>")
+def tester(kata, angka, terserah):
+    return render_template('tester.html', kata=kata, angka=angka, terserah=terserah)
+    
 @app.route("/calc")
 def calc():
-    number1 = int(request.args.get('number'))
-    number2 = int(request.args.get('numbers'))
-    operator = request.args.get('operator')
-    result = None
+    number1 = int(request.args.get('n1'))
+    number2 = int(request.args.get('n2'))
+    operator = request.args.get('op')
+    result = number1+number2
     if operator == "+":
         result = number1+number2
     elif operator == "-":
@@ -34,4 +49,3 @@ def calc():
 
 if __name__ == '__main__':
     app.run(debug=True)
-    
